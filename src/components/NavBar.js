362b50e46c4ab8fe46c3bbd9f21ece2b930e45fd
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-07-14 15:13:27 +0300

    navbar complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,22 @@
 import React from "react";
 import "../style.css"
-
+import { RiGithubLine } from 'react-icons/ri'
+import { AiOutlineHome, AiOutlineFundProjectionScreen } from 'react-icons/ai'
+import { BsFillPersonFill } from "react-icons/bs"
+import { BiPencil } from "react-icons/bi"
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <div className="container">
-
-      </div>
-    </nav>
+    <div className="nav-bar-container">
+      <a className="logo" href="/">Ut.</a>
+      <nav className="navbar">
+        <a className="nav-buttons"  href="/" ><AiOutlineHome size={20} style={{padding: 2}}/>Home</a>
+        <a className="nav-buttons" href="/"><BsFillPersonFill size={20} style={{padding: 2}}/>About</a>
+        <a className="nav-buttons" href="/"><AiOutlineFundProjectionScreen size={20} style={{padding: 2}}/>Projects</a>
+        <a className="nav-buttons" href="/"><BiPencil size={20} style={{padding: 2}}/>Resume</a>
+        <div className="nav-buttons" href="/"><RiGithubLine size={24} style={{padding: 2}} />GitHub</div>
+      </nav >
+    </div >
   );
 }
 
