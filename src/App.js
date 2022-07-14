4cb6dd2f6acf85b5462cc65cdab6174a42d1f930
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-07-14 09:50:46 +0300

    Fresh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+import "./style.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <div>
+   <NavBar/>
+   </div>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../style.css"
+
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
