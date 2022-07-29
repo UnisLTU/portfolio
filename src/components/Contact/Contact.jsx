5af6ae2ac8cf1c556ac2ffba8e78b135ab1d0a4c
UnisLTU <109076072+UnisLTU@users.noreply.github.com>
2022-07-29 14:23:44 +0300

    Complete portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ import { RiGithubLine } from 'react-icons/ri'
 function Contact() {
     return (
         <>
-            <div className={styles.contact_header}> Contact Me!
+            <div className={styles.contact_header} id="contact"> Contact Me!
                 <div style={{ display: "flex", flexDirection: "row", width: "100%", justifyContent: "center" }}>
                     <a href="linkedin.com/in/ugnius-tyla-9083a1132" className={styles.contact_card}>
                         <AiOutlineLinkedin size={32} style={{ padding: 8 }} />
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import styles from "./Projects.module.css"
+import portfolio from '../../Assets/port.PNG';
 
 function Projects() {
   return (
     <>
       <div className={styles.projects_container} id="projects">
-        Ongoing projects:
+        Projects' timeline:
+      </div>
+      <div className={styles.project_name}> My first portfolio</div>
+      <div className={styles.project_date}>2022.08</div>
+      <div className={styles.divider_container}>
+      <img src={portfolio} alt="image1" />
+        <div className={styles.divider}></div>
+        <div className={styles.text_container}>
+          <p>My first personal portfolio website created with React.js and CSSModules.
+          </p>
+          <a className={styles.alternate1} href="/">To Project</a>
+          <div className={styles.frameworks}> React | CSSModules </div>
+        </div>
+      </div>
+      <div className={styles.project_name}>Second project</div>
+      <div className={styles.project_date}>2022....</div>
+      <div className={styles.divider_container}>
+        <div className={styles.text_container}>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae perferendis sint molestias consequuntur dolorum blanditiis quaerat doloribus harum distinctio, quo, laborum enim, dolores tempore? In libero excepturi consequatur molestias a.</p>
+          <a className={styles.alternate2} href="/">To Project</a>
+          <div className={styles.frameworks}> React | CSSModules </div>
+        </div>
+        <div className={styles.divider}></div>
+        <img src={portfolio} alt="image1" />
       </div>
     </>
   )
